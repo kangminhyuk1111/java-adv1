1b5f1fb221da208ac41e8a1fb1e513ccb1aae223
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-08-13 16:59:14 +0900

    interrupt

---
+++ src/thread/control/interrupt/ThreadStopMainV1.java
@@ -0,0 +1,40 @@
+package thread.control.interrupt;
+
+import static thread.control.ThreadUtils.sleep;
+import static util.MyLogger.log;
+
+public class ThreadStopMainV1 {
+
+    public static void main(String[] args) {
+        final MyTask task = new MyTask();
+
+        final Thread thread = new Thread(task, "work");
+
+        thread.start();
+
+        sleep(4000);
+        log("작업 중단 지시 runFlag = false");
+        thread.interrupt();
+        log("work 스레드 인터럽트 상태1 = " + thread.isInterrupted());
+    }
+
+    static class MyTask implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    log("작업 중");
+                    Thread.sleep(3000);
+
+                }
+            } catch (InterruptedException e) {
+                log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());
+                log("interrupt message = " + e.getMessage());
+                log("state = " + Thread.currentThread().getState());
+            }
+            log("자원 정리");
+            log("자원 종료");
+        }
+    }
+}
