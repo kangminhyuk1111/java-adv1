2418eecfdefbbbf71a59ca0c5a3d0f4078d219f0
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-08-14 16:52:34 +0900

    interrupt

--- src/thread/control/interrupt/ThreadStopMainV3.java
+++ src/thread/control/interrupt/ThreadStopMainV3.java
@@ -3,7 +3,7 @@ package thread.control.interrupt;
 import static thread.control.ThreadUtils.sleep;
 import static util.MyLogger.log;
 
-public class ThreadStopMainV1 {
+public class ThreadStopMainV3 {
 
     public static void main(String[] args) {
         final MyTask task = new MyTask();
@@ -14,7 +14,7 @@ public class ThreadStopMainV1 {
 
         // Interrupt
         // 프로세스 실행 도중 예기치 않은 상황이 발생할 때 발생한 상황을 처리한 후, 실행중인 작업으로 복귀하는 것을 말한다.
-        sleep(4000);
+        sleep(100);
         thread.interrupt();
         log("work 스레드 인터럽트 상태1 = " + thread.isInterrupted());
     }
@@ -23,16 +23,11 @@ public class ThreadStopMainV1 {
 
         @Override
         public void run() {
-            try {
-                while (true) {
-                    log("작업 중");
-                    Thread.sleep(3000);
-                }
-            } catch (InterruptedException e) {
-                log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());
-                log("interrupt message = " + e.getMessage());
-                log("state = " + Thread.currentThread().getState());
+            while (!Thread.currentThread().isInterrupted()) {
+                log("작업 중");
             }
+            log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());
+
             log("자원 정리");
             log("자원 종료");
         }
